0f93e60893f37fdd4728d84482f4b69cae5f0a57
leopaccela <leopaccela@gmail.com>
2021-12-04 20:11:45 -0300

    getGifts modified

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifts = async(category)=>{
 
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=pFN4ATj9wS3n9Ztr23Wad6P1YR5EqJrs`
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=8&api_key=pFN4ATj9wS3n9Ztr23Wad6P1YR5EqJrs`
     const response = await fetch(url)
     const {data} = await response.json()
 
